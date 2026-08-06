53325e8eca293b3da42aa3592d2c656de463e1cf
Ronald8595 <ronaldagurto94@gmail.com>
2026-08-06 16:39:33 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <section id='center'>
+      <h1>Monitoreo Ambiental</h1>
+    </section>
+    </>
+    
+  )
+}
+
+export default App
